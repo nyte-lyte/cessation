7cf17353e1f82605f9b965d13be9cd5c00c9f923
Jess <hillyerjess@gmail.com>
2024-08-26 18:51:38 -0800

    decay_logic.js

--- data/decay_logic.js
+++ data/decay_logic.js
@@ -1,12 +1,41 @@
-let baseDecayRate = 0.01;
+let baseDecayRate = 0.01; 
+
+function calculateDynamicBaseDecayRate(dataSet) {
+    
+    let normalizedQTc = normalize(dataSet.ecg.qtcInterval, minMaxValues.qtcInterval.min, minMaxValues.qtcInterval.max);
+    let normalizedCreatinine = normalize(dataSet.labs.creatinine, minMaxValues.creatinine.min, minMaxValues.creatinine.max);
+    let normalizedEGFR = normalize(dataSet.labs.eGFR, minMaxValues.eGFR.min, minMaxValues.eGFR.max);
+    let normalizedGlucose = normalize(dataSet.labs.glucose, minMaxValues.glucose.min, minMaxValues.glucose.max);
+
+    
+    let weightQTc = 0.4; 
+    let weightCreatinine = 0.3;
+    let weightEGFR = 0.2;
+    let weightGlucose = 0.1;
+
+    
+    let dynamicBaseDecayRate = (
+        normalizedQTc * weightQTc +
+        normalizedCreatinine * weightCreatinine +
+        (1 - normalizedEGFR) * weightEGFR + 
+        normalizedGlucose * weightGlucose
+    );
+
+    
+    return dynamicBaseDecayRate;
+}
 
 function updateDecayRate(dataSet) {
+  
+  let dynamicBaseDecayRate = calculateDynamicBaseDecayRate(dataSet);
+
   let healthIndex = calculateHealthIndex(dataSet);
 
-  let adjustedDecayRate = baseDecayRate * (1 + (1 - healthIndex));
+  
+  let adjustedDecayRate = dynamicBaseDecayRate * (1 + (1 - healthIndex));
 
-  if (adjustedDecayRate < baseDecayRate) {
-    adjustedDecayRate = baseDecayRate;
+  if (adjustedDecayRate < dynamicBaseDecayRate) {
+    adjustedDecayRate = dynamicBaseDecayRate;
   }
 
   console.log(
@@ -20,6 +49,8 @@ function applyDecay(rate) {
   console.log(`Applying decay at rate: ${rate}`);
 }
 
+
 healthDataSets.forEach((dataSet) => {
   updateDecayRate(dataSet);
 });
+
